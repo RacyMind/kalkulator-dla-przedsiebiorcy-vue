36eabdb33c2faad836a06fa3638a89a04051e3c6
Łukasz Socha <l.socha@sopamo.de>
2021-04-24 16:42:36 +0200

    fix date

--- src/components/changeLogs/logs.js
+++ src/components/changeLogs/logs.js
@@ -2,7 +2,7 @@ export default {
   LOGS: [
     {
       version: '3.0.0',
-      publish_date: '2021-04-26',
+      publish_date: '2021-04-24',
       content: '<ul><li>Aplikacja na nowo napisana w VueJS</li><li>Poprawiono liczne błędy</li><li>Poprawiono UX</li><li>Dodano zestawienia roczne dla wynagrodzenia</li></ul>',
     },
   ],
