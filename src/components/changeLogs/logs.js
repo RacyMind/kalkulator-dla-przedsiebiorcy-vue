cb28583ed0042c621730507cf2f8867a529e9c24
Łukasz Socha <l.socha@sopamo.de>
2021-04-25 16:24:19 +0200

    add PWA

--- src/components/changeLogs/logs.js
+++ src/components/changeLogs/logs.js
@@ -1,6 +1,11 @@
 export default {
   LOGS: [
     {
+      version: '3.0.1',
+      publish_date: '2021-04-25',
+      content: '<ul><li>Dodano PWA. Aplikację można  zainstalować w telefonie.</li></ul>',
+    },
+    {
       version: '3.0.0',
       publish_date: '2021-04-24',
       content: '<ul><li>Aplikacja na nowo napisana w VueJS</li><li>Poprawiono liczne błędy</li><li>Poprawiono UX</li><li>Dodano zestawienia roczne dla wynagrodzenia</li></ul>',
