3db1e1b8f97bf65350d05c7fc432453dd98fa0f8
Łukasz Socha <l.socha@sopamo.de>
2021-10-24 20:05:47 +0200

    remove csvToJson

--- src/logic/helpers.js
+++ src/logic/helpers.js
@@ -42,35 +42,8 @@ function getDefaultYear () {
 
   return constants.AVAILABLE_YEARS[constants.AVAILABLE_YEARS.length - 1]
 }
-
-function csvToJson (csv) {
-  const lines = csv.split('\n')
-
-  const result = []
-
-  // NOTE: If your columns contain commas in their values, you'll need
-  // to deal with those before doing the next step
-  // (you might convert them to &&& or something, then covert them back later)
-  // jsfiddle showing the issue https://jsfiddle.net/
-  const headers = lines[0].split(';')
-
-  for (let i = 1; i < lines.length; i++) {
-    const obj = {}
-    const currentline = lines[i].split(';')
-
-    for (let j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j]
-    }
-
-    result.push(obj)
-  }
-
-  // return result; //JavaScript object
-  return JSON.stringify(result) // JSON
-}
 export default {
   scrollToElement,
   round,
   getDefaultYear,
-  csvToJson,
 }
