8e98441b11cf6c69eddb7be8cdd49ed1ab66594c
Łukasz Socha <l.socha@sopamo.de>
2021-12-21 15:08:35 +0100

    refactor VatLimit

---
+++ src/components/vatLimit/Summary.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <ListRow
+      name="Data rozpoczęcia działalności"
+      :value="formatDate(result.startDate)"
+    />
+    <ListRow
+      class="bg-teal-1"
+      name="Liczba dni działalności do końca roku"
+      :value="result.daysToEndYear"
+    />
+    <ListRow
+      class="bg-primary text-white"
+      name="Limit przychodu uprawniający do zwolnienia"
+      :value="pln(result.amount)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import {computed, PropType, Ref, toRefs} from 'vue'
+import { format } from 'date-fns'
+import { pln } from 'src/use/currencyFormat'
+import vatLimit from 'components/vatLimit/vatLimit'
+import ListRow from 'components/partials/ListRow.vue'
+import {VatLimitInputFields} from 'components/vatLimit/interfaces/VatLimitInputFields'
+import {VatLimitResult} from 'components/vatLimit/interfaces/VatLimitResult'
+
+export default {
+  props: {
+    input: {
+      type: Object as PropType<VatLimitInputFields>,
+      required: true,
+    },
+  },
+  setup (props:any) {
+    const { input } = toRefs(props)
+
+    const result:Readonly<Ref<Readonly<VatLimitResult>>> = computed(() => {
+      try {
+        return vatLimit.getResult(input.value)
+      }
+      catch {
+        return {
+          startDate: null,
+          daysToEndYear: 0,
+          amount: 0,
+        }
+      }
+    })
+
+    const formatDate = (date:Date|null) => {
+      if (!date) {
+        return null
+      }
+      return format(date, 'dd.MM.yyyy')
+    }
+
+    return {
+      result,
+      pln,
+      formatDate,
+    }
+  },
+  components: {
+    ListRow,
+  },
+}
+</script>
--- src/pages/VatLimit.vue
+++ src/pages/VatLimit.vue
@@ -10,40 +10,58 @@
       </SectionHeader>
       <Form
         class="q-mt-md q-mb-lg q-px-md"
-        @scroll="scrollTo"
+        @save="save"
       />
       <Advert />
       <SectionHeader ref="scrollTarget">
         <q-icon name="o_credit_card" />
         Podsumowanie
       </SectionHeader>
-      <Table />
+      <Summary
+        :input="inputFields"
+      />
     </div>
     <Footer />
   </q-page>
 </template>
 
-<script>
-import SectionHeader from 'components/partials/SectionHeader'
-import Form from 'components/vatLimit/Form'
-import Table from 'components/vatLimit/Table'
-import Advert from 'components/partials/Advert'
-import Footer from 'components/Footer'
+<script lang="ts">
+import {useStore} from 'vuex'
+import {ref} from 'vue'
+import SectionHeader from 'components/partials/SectionHeader.vue'
+import Form from 'components/vatLimit/Form.vue'
+import Summary from 'components/vatLimit/Summary.vue'
+import Advert from 'components/partials/Advert.vue'
+import Footer from 'components/Footer.vue'
 import helpers from 'src/logic/helpers'
+import {VatLimitInputFields} from 'components/vatLimit/interfaces/VatLimitInputFields'
 
 export default {
-  created () {
-    this.$store.commit('app/setModuleTitle', 'Limit sprzedaży dla zwolnienia z VAT')
-  },
-  methods: {
-    scrollTo () {
-      helpers.scrollToElement(this.$refs.scrollTarget.$el)
-    },
+  setup() {
+    const store = useStore()
+    store.commit('app/setModuleTitle', 'Limit sprzedaży dla zwolnienia z VAT')
+
+    const inputFields = ref(<VatLimitInputFields>{
+      startDate: null,
+    })
+
+    const scrollTarget = ref(null) as any
+
+    const save = (input: VatLimitInputFields) => {
+      inputFields.value = input
+      helpers.scrollToElement(scrollTarget?.value?.$el)
+    }
+
+    return {
+      inputFields,
+      scrollTarget,
+      save,
+    }
   },
   components: {
     SectionHeader,
     Form,
-    Table,
+    Summary,
     Advert,
     Footer,
   },
