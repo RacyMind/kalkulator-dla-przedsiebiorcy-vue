851ea75c7afeba1a54788b2a2e0bb23099208f96
Łukasz Socha <l.socha@sopamo.de>
2021-12-30 19:16:40 +0100

    change the text in Advert

--- src/components/partials/Advert.vue
+++ src/components/partials/Advert.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="text-center bg-teal-1 q-py-md">
     <div>
-      Pomóż sfinansować wsparcie księgowego
+      Pomóż sfinansować audyt wykonany księgowego
     </div>
     <q-btn
-      label="Pomagam"
+      label="Wpłać na zrzutka.pl"
       no-caps
       class="q-mt-sm"
       style="width: 75%;max-width:180px"
