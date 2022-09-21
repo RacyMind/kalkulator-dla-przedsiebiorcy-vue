90a5af70039debbe932132cf28f50e41fadd880f
Łukasz Socha <l.socha@sopamo.de>
2022-09-21 16:31:48 +0200

    fix logs

--- src/components/changeLogs/logs.ts
+++ src/components/changeLogs/logs.ts
@@ -1,7 +1,7 @@
 export default [
   {
-    content: '<ul><li>Refaktoryzacja modułu Kursy walut.</li><li>Refaktoryzacja modułu Przelicznik walut.</li></ul>',
-    publish_date: '2022-07-09',
+    content: '<ul><li>Refaktoryzacja modułu Kursy walut.</li><li>Refaktoryzacja modułu Przelicznik walut.</li><li>Aktualizacja odsetek ustawowych.</<li></ul>',
+    publish_date: '2022-09-21',
     version: '4.9.0',
   },
   {
