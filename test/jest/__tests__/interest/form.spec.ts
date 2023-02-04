7aac0ad43f1cfb8489b17d965aa3f018d877671b
dithh <aleksander.hersztek@gmail.com>
2023-02-04 15:37:38 +0100

    feature/interest form component test

--- test/jest/__tests__/interest/form.spec.ts
+++ test/jest/__tests__/interest/form.spec.ts
@@ -2,6 +2,7 @@ import {describe, expect,it} from '@jest/globals'
 import {installQuasarPlugin} from '@quasar/quasar-app-extension-testing-unit-jest'
 import {mount} from '@vue/test-utils'
 import Form from 'components/interest/Form.vue'
+import constants from 'src/logic/constants'
 
 installQuasarPlugin()
 
@@ -12,14 +13,14 @@ describe('Interest form',()=>{
     const wrapper = mount(Form)
     await wrapper.find('[data-testid="basic-capital-rate"]').trigger('click')
     const input = wrapper.find('[data-testid="rate"]').element as HTMLInputElement
-    expect(input.value).toBe('10.25')
+    expect(Number(input.value)).toBe(constants.BASIC_CAPITAL_INTEREST_RATE)
   })
 
   it('basic late rate  toggle should set rate to 12.25',async ()=>{
     const wrapper = mount(Form)
     await wrapper.find('[data-testid="basic-late-rate"]').trigger('click')
     const input = wrapper.find('[data-testid="rate"]').element as HTMLInputElement
-    expect(input.value).toBe('12.25')
+    expect(Number(input.value)).toBe(constants.BASIC_LATE_INTEREST_RATE)
   })
 
 })
