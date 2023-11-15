efbc5a1ac0f519771b809d5af4019655513dad60
Łukasz Socha <l.socha@sopamo.de>
2023-11-15 18:45:17 +0100

    refactor adverts

--- src/components/partials/Advert.vue
+++ src/components/partials/Advert.vue
@@ -1,44 +1,19 @@
 <template>
   <div class="text-center bg-teal-1 q-py-md">
-    <!--
-     <div>
-        Wesprzyj rozwój aplikacji
-      </div>
-      <q-btn
-        label="Wpłać na zrzutka.pl"
-        no-caps
-        class="q-mt-sm"
-        style="width: 75%;max-width:180px"
-        color="primary"
-        outline
-        type="a"
-        target="_blank"
-        href="https://zrzutka.pl/z/auddyt"
-      />
-    -->
-    <div>
-      Wesprzyj twórcę aplikacji i przekaż 1,5% podatku<br>
-      KRS: 0000270809<br>
-      Cel szczególny: Socha, 15548
-    </div>
-    <q-btn
-      label="Więcej informacji"
-      no-caps
-      class="q-mt-sm"
-      style="width: 75%;max-width:180px"
-      color="primary"
-      outline
-      @click="openModal = true"
-    />
-    <q-dialog v-model="openModal">
-      <SupportAuthor />
-    </q-dialog>
+    <TaxDonation v-if="isTaxDonationTimeFrame() && Math.random() < 0.5" />
+    <Donate v-else />
   </div>
 </template>
 <script lang="ts" setup>
 import {ref} from 'vue'
+import Donate from 'components/partials/adrverts/Donate.vue'
 import SupportAuthor from 'components/partials/SupportAuthor.vue'
+import TaxDonation from 'components/partials/adrverts/TaxDonation.vue'
 
 const openModal = ref(false)
 
+const isTaxDonationTimeFrame = () => {
+  const nmonth = new Date().getMonth()
+  return nmonth > 1 && nmonth < 5
+}
 </script>
---
+++ src/components/partials/adrverts/TaxDonation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      Wesprzyj twórcę aplikacji i przekaż 1,5% podatku<br>
+      KRS: 0000270809<br>
+      Cel szczególny: Socha, 15548
+    </div>
+    <q-btn
+      label="Więcej informacji"
+      no-caps
+      class="q-mt-sm"
+      style="width: 75%;max-width:180px"
+      color="primary"
+      outline
+      @click="openModal = true"
+    />
+    <q-dialog v-model="openModal">
+      <SupportAuthor />
+    </q-dialog>
+  </div>
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue'
+import SupportAuthor from 'components/partials/SupportAuthor.vue'
+
+const openModal = ref(false)
+</script>
