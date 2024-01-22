5e45011950a3a6968fabcb3be61bac292da3cf17
Łukasz Socha <l.socha@sopamo.de>
2024-01-22 16:46:55 +0100

    implementing the Accounting with Spouse

---
+++ src/components/accountingWithSpouse/interfaces/CustomResult.ts
@@ -0,0 +1,9 @@
+export interface CustomResult{
+  readonly revenue: number
+  readonly socialContributions: number
+  readonly healthContributions: number
+  readonly expenses: number
+  readonly taxBasis: number
+  readonly taxAmount: number
+  readonly income: number
+}
