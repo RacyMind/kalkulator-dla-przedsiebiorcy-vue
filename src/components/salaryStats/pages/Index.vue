6e1feb11c05a3969e843c3186c6788c0a6449660
Łukasz Socha <l.socha@sopamo.de>
2024-02-01 16:58:06 +0100

    refactor SalaryStats

---
+++ src/components/salaryStats/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <ModulePageLayout class="c-informator">
+    <FormSection
+      v-if="availableDates.length > 1"
+      title="Data obowiązywania przepisów">
+      <LawRuleDate />
+    </FormSection>
+    <Advert/>
+    <MinimumWageSummary />
+  </ModulePageLayout>
+</template>
+
+<script setup lang="ts">
+
+import {useBreadcrumbStore} from 'stores/breadcrumbStore'
+import {useLawRuleDate} from 'src/composables/lawRuleDate'
+import Advert from 'components/partials/Advert.vue'
+import FormSection from 'components/partials/form/FormSection.vue'
+import LawRuleDate from 'components/partials/LawRuleDate.vue'
+import MinimumWageSummary from 'components/salaryStats/components/MinimumWageSummary.vue'
+import ModulePageLayout from 'components/partials/ModulePageLayout.vue'
+
+const { availableDates } = useLawRuleDate()
+
+const breadcrumbStore = useBreadcrumbStore()
+breadcrumbStore.items = [
+  {
+    name: 'Informacje o wynagrodzeniu',
+  },
+]
+</script>
