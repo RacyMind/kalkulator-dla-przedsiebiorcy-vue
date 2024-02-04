a4d5f944a882db1bd6765be98252157ce6ca0b1c
Łukasz Socha <l.socha@sopamo.de>
2024-02-04 18:39:02 +0100

    WIP: implementing the real bought costs module

---
+++ src/components/realBoughtCosts/logic/RealCostCalculator.ts
@@ -0,0 +1,31 @@
+import {BasicCalculator} from 'src/logic/BasicCalculator'
+import {Calculator} from 'src/logic/interfaces/Calculator'
+import {InputFields} from 'components/realBoughtCosts/interfaces/InputFields'
+import {Result} from 'components/realBoughtCosts/interfaces/Result'
+import helpers from 'src/logic/helpers'
+
+export class ContributionCalculator extends BasicCalculator<InputFields, Result> implements Calculator<InputFields, Result>{
+
+  constructor() {
+    super()
+  }
+
+  public calculate(): this {
+    const priceWithoutTax = helpers.round(this.getInputData().price / (1 + this.getInputData().vatTaxRate), 2)
+    const vatTaxAmount = helpers.round(this.getInputData().price - priceWithoutTax, 2)
+    const deductedVatTaxAmount = helpers.round(vatTaxAmount * this.getInputData().deductedVatTaxPart, 2)
+    const deductedIncomeTaxAmount = helpers.round(priceWithoutTax * this.getInputData().incomeTaxRate, 2)
+
+    const savedAmount = helpers.round(deductedVatTaxAmount + deductedIncomeTaxAmount, 2)
+
+    this.result = {
+      price: this.getInputData().price,
+      vatTaxAmount,
+      deductedVatTaxAmount,
+      deductedIncomeTaxAmount,
+      savedAmount,
+    }
+
+    return this
+  }
+}
