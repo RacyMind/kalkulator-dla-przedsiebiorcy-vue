f0882cdbd8167394b752eeced4103c374b8b4c85
Łukasz Socha <l.socha@sopamo.de>
2024-02-12 18:03:49 +0100

    clean up the code

--- src/components/salaryForUnusedHolidays/interfaces/SalaryForUnusedHolidaysFields.ts
+++
@@ -1,9 +0,0 @@
-import {AvailableYear} from 'src/types/AvailableYear'
-
-export interface SalaryForUnusedHolidaysFields {
-  amount: number
-  dailyNorm: number
-  holidayHours: number
-  year: AvailableYear,
-  workingTime: number
-}
