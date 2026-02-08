f537c0833888ecc85124dc4484f1567b67b97f71
Lukasz Socha <l.socha@sopamo.de>
2026-02-08 18:17:19 +0100

    chore: add analytics for Android

--- src/logic/analytics.ts
+++ src/logic/analytics.ts
@@ -1,23 +1,34 @@
-import { uid } from 'quasar'
+import { uid } from 'quasar';
+import { Capacitor } from '@capacitor/core';
+import { FirebaseAnalytics } from '@capacitor-firebase/analytics';
 
 declare global {
   interface Window {
-    dataLayer:any;
+    dataLayer: any;
   }
 }
 
+const isNative = Capacitor.isNativePlatform();
+
 export default {
-  getCid () {
-    // We need an unique identifier for this session
-    // We store it in a localStorage, but you may use cookies, too
+  getCid() {
     if (!localStorage.cid) {
-      localStorage.cid = uid()
+      localStorage.cid = uid();
     }
-    return localStorage.cid
+    return localStorage.cid;
   },
 
-  logEvent (category:string, action:string, label:string, value = null) {
-    if(!process.env.DEV) {
+  logEvent(category: string, action: string, label: string, value = null) {
+    if (process.env.DEV) {
+      return;
+    }
+
+    if (isNative) {
+      FirebaseAnalytics.logEvent({
+        name: action,
+        params: { category, label, value, cid: this.getCid() },
+      });
+    } else {
       window.dataLayer.push({
         action: action,
         category: category,
@@ -25,18 +36,26 @@ export default {
         event: 'customEvent',
         label: label,
         value: value,
-      })
+      });
     }
   },
 
-  logPage (path:string) {
-    if(!process.env.DEV) {
+  logPage(path: string) {
+    if (process.env.DEV) {
+      return;
+    }
+
+    if (isNative) {
+      FirebaseAnalytics.logEvent({
+        name: 'screen_view',
+        params: { screen_name: path, cid: this.getCid() },
+      });
+    } else {
       window.dataLayer.push({
         cid: this.getCid(),
         event: 'customPageView',
         path: path,
-      })
+      });
     }
   },
-
-}
+};
